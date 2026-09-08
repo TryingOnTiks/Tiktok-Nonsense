1c1d7dfaa804dcf082f1f99342a0068ccb9fe10b
Jojay De Castro <decastrojojay2004@gmail.com>
2026-09-08 18:12:44 +0800

    Added deletion function

--- helpdesk.py
+++ helpdesk.py
@@ -26,7 +26,8 @@ def view_tickets():
     print("=======View Tickets======")
     if len(tickets) == 0:
         print("No tickets")
-
+        return
+    
     for ticket in tickets:
         print(f"Ticket #{ticket['id']}")
         print(f"Name: {ticket['name']}")
@@ -34,11 +35,37 @@ def view_tickets():
         print(f"Priority: {ticket['priority']}")
         print(f"Status: {ticket['status']}\n")
 
+def delete_ticket():
+    print("\n======Delete Ticket=====")
+    if len(tickets) == 0:
+        print("No tickets")
+        return
+
+    print("Available Tickets:")
+
+    for ticket in tickets:
+        print(f"Ticket #{ticket['id']} - {ticket['name']}")
+
+    try:
+        ticket_id = int(input("Enter ticket number to delete: "))
+
+        for i, ticket in enumerate(tickets):
+            if ticket["id"] == ticket_id:
+                del tickets[i]
+                print(f"Ticket #{ticket_id} deleted.")
+                return
+
+        print("Ticket not found.")
+
+    except ValueError:
+        print("Enter VALID ticket number!")
+
 while True:
     print("=====IT HELP DESK=====")
     print("1. Create ticket")
     print("2. View Tickets")
-    print("3. Exit\n")
+    print("3. Ticket Delete")
+    print("4. Exit\n")
     choice = input("Option: (1-3): ")
 
     if choice == "1":
@@ -46,6 +73,8 @@ while True:
     elif choice == "2":
         view_tickets()
     elif choice == "3":
+        delete_ticket()
+    elif choice == "4":
         print("Exiting....")
         time.sleep(1)
         break
